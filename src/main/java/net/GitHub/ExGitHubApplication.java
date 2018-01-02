a7020d2aa4dd1f4f2cc6ca672a4bc19d06adac4c
INSEOK-PC\Seo <insuk_seo@hotmail.com>
2018-01-02 11:41:19 +0900

    ㅋㅋㄴㅁㅋㅋㅋㅋㅋ

---
+++ src/main/java/net/GitHub/ExGitHubApplication.java
@@ -0,0 +1,12 @@
+package net.GitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExGitHubApplication.class, args);
+	}
+}
---
+++ src/test/java/net/GitHub/ExGitHubApplicationTests.java
@@ -0,0 +1,16 @@
+package net.GitHub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExGitHubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
